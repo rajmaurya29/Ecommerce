f67985b4eeb7392ff2f721c951bc16fd62a6b91c
rajmaurya29 <rajmaurya29>
2025-03-18 18:58:00 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap'
+import HomeScreen  from './screens/HomeScreen'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header/>
+          <main className="py-3">
+            <Container>
+              <HomeScreen/>
+              <h1>Welcome</h1>
+            </Container>
+          </main>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
