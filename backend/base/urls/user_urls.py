5a3e0545478f0d407efe018e04430707fba65ca6
Raj Maurya <rajmaurya29092004@gmail.com>
2026-02-25 08:22:20 +0530

    implemented forgot password/register in backend

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -9,11 +9,15 @@ urlpatterns = [
     path('logout/', user_views.logoutUser, name='logout_user'),
     path('profile/', user_views.getUser, name='users-profile'),
     path('update/', user_views.updateUser, name='users-update'),
-    path('', user_views.getUsers, name='users'),
+    
     path('register/', user_views.registerUsers, name='users-register'),
     path('profiletoken/', user_views.getUserWithToken, name='users-profileToken'),
     path('delete/<str:id>/', user_views.deleteUser, name='users-deleteUser'),
     path('editUser/<str:id>/', user_views.getUserById, name='users-getUserById'),
     path('admin/<str:id>/', user_views.updateUserAdmin, name='users-updateUserAdmin'),
-    path('health/',user_views.health_check,name='health_check')
+    path('health/',user_views.health_check,name='health_check'),
+    path('forgot-password/',user_views.forgot_password,name='forgot-password'),
+    path('reset-password/',user_views.reset_password,name='reset-password'),
+    path('verify-email/',user_views.verify_email,name='verify-email'),
+    path('', user_views.getUsers, name='users'),
 ]
